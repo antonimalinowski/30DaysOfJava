4cc0e05a7c55000ab1a40806db3f628475e6ce75
antek <antonimalinowski.kontakt@gmail.com>
2021-12-03 09:03:22 +0100

    Convert queue to stack

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,22 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MySweetProgram {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner in = new Scanner(new File("./src/students.txt"));
+	public static void main(String[] args) {
+		LinkedList<String> names = new LinkedList<String>();
+		System.out.println("Queue: ");
+		names.add("Santiago");
+		names.add("Christof");
+		names.add("Bernard");
+		names.add("Bernie");
+		names.add("Bernard");
+		System.out.println(names.removeFirst());
+		System.out.println(names.remove(3));
+		System.out.println("\n");
+		names.clear();
+		names.add("Marty");
+		names.add("Carter");
+		System.out.println(names.remove());
+		System.out.println("\n");
 		
-		List<String> students = new ArrayList<String>();
-		while(in.hasNextLine()) {
-			students.add(in.nextLine());
-		}
-		
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("name: " + students.get(i));
-		}
-		in.close();
+		System.out.println("Stack: ");
+		names.push("Santiago2");
+		names.push("Christof2");
+		names.push("Bernard2");
+		System.out.println(names.pop());
+		System.out.println(names.pop());
+		System.out.println("\n");
+		names.clear();
+		names.push("Marty2");
+		names.push("Carter2");
+		System.out.println(names.pop());
 	}
 }
